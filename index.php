eebfcaf5f9181f3b7d262bcb672c17c752ff779c
Michael Romero <m.romero1989@gmail.com>
2017-11-05 14:40:46 -0700

    Added jumbotron.

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<script src="https://cdnjs.cloudfare.com/ajax/libs/jquery/3.2.1/jquery/.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"  integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" 	integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+		<title>Bootstrap Challenge</title>
+	</head>
+
+	<body>
+		<header>
+			<div class="container">
+				<nav class="navbar navbar-expand-md navbar-light bg-light border border-dark rounded mt-4">
+					<div class="collapse navbar-collapse" id="navbarText">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="#">Title<span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link1</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link2</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link3</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link4</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+				<div class="container">
+					<div class="jumbotron mt-5">
+						<h1 class="display-3">Hello, world!</h1>
+						<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra 						attention to featured content or information.</p>
+						<p class="lead">
+							<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+						</p>
+					</div>
+				</div>
+		</header>
+	</body>
+</html>
